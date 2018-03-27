8217c91bb24632960fe07bae1a7e71ac75d6941e
Amysheng123 <36951533+Amysheng123@users.noreply.github.com>
2018-03-27 12:20:11 +0800

    Update WebDriver2.java
    调用version方法

--- src/main/java/WebDriver2.java
+++ src/main/java/WebDriver2.java
@@ -8,6 +8,7 @@ public class WebDriver2 {
     public static void main(String[] args) {
         WebDriver driver = new FirefoxDriver();
         driver.navigate().to("http://www.baidu.com");
+        version();
     }
     public void version(){
         //感谢关注云层天咨微信公众号及腾讯课堂
