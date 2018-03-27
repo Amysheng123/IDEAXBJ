ba786edcc025dfed22741eb9fa9dfa73cb497658
amy sheng <123940664@qq.com>
2018-03-27 12:11:40 +0800

    添加关于提交给github的基础代码结构

---
+++ src/main/java/WebDriver2.java
@@ -0,0 +1,12 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+/**
+ * Created by amy sheng on 3/27/2018.
+ */
+public class WebDriver2 {
+    public static void main(String[] args) {
+        WebDriver driver = new FirefoxDriver();
+        driver.navigate().to("http://www.baidu.com");
+    }
+}
